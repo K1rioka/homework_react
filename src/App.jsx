e1194b722d10d537754f9cbe07fe0be079fcdf00
K1rioka <eshopokov@gmail.com>
2023-12-26 22:40:00 +0630

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import React, { useState, useContext, useRef } from 'react';
+import { Route, Routes, BrowserRouter, Link } from 'react-router-dom';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import Contact from './components/Contact';
+
+const ThemeContext = React.createContext();
 
 const NotFound404 = () => {
   return (
@@ -13,16 +15,83 @@ const NotFound404 = () => {
   );
 };
 
+const TextInput = () => {
+  const inputRef = useRef(null);
+  const [text, setText] = useState('');
+
+  const handleClear = () => {
+    inputRef.current.value = '';
+    setText('');
+  };
+
+  const handleInputChange = () => {
+    setText(inputRef.current.value);
+  };
+
+  return (
+    <div>
+      <input type="text" ref={inputRef} onChange={handleInputChange} />
+      <button onClick={handleClear}>Clear</button>
+      <label>{text}</label>
+    </div>
+  );
+};
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+const Header = () => {
+  const { toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <header>
+      <h1>Theme App</h1>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+    </header>
+  );
+};
+
+const Content = () => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <div style={{ color: theme === 'light' ? 'black' : 'white' }}>
+      <p>This is some content.</p>
+    </div>
+  );
+};
+
 const App = () => {
   return (
-  <BrowserRouter>
-    <Routes>
-        <Route path="/" component={<Home/>} />
-        <Route path="/about" component={<AboutUs/>} />
-        <Route path="/contact" component={<Contact/>} />
-        <Route component={NotFound404} />
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/ref" element={<TextInput />} />
+        <Route
+          path="/context"
+          element={
+            <ThemeProvider>
+              <Header />
+              <Content />
+            </ThemeProvider>
+          }
+        />
+        <Route path="*" element={<NotFound404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
